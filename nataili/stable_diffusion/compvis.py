d5f383b7f6cde2d44bb75ab5c854c6582738ab38
Donald Nixon <donald@nodero.com>
2023-03-02 20:29:28 +1300

    Revert "Initial re-update"
    This reverts commit 9779502aa59875992cabc701a54fc923493f8e1c.

--- nataili/stable_diffusion/compvis.py
+++ nataili/stable_diffusion/compvis.py
@@ -574,10 +574,7 @@ class CompVis:
                 prompt_tokens = re.findall("<([a-zA-Z0-9-]+)>", prompt)
                 if prompt_tokens:
                     process_prompt_tokens(
-                        prompt_tokens, 
-                        model if control_type is None else self.control_net_model, 
-                        self.concepts_dir,
-                        self.model_baseline
+                        prompt_tokens, model if control_type is None else self.control_net_model, self.concepts_dir
                     )
 
             all_prompts = batch_size * n_iter * [prompt]
